8876b3ec9ccad82727036c527cff17ec2a625ca4
alistairwalsh <alistair.walsh@me.com>
2016-06-06 14:04:49 +1000

    Develop challenges

--- Challenges/ex1.py
+++ Challenges/ex1.py
@@ -1,4 +1,1 @@
 # write greetings.py
-
-# it should ask for a name 
-# and then print out a greeting
--- Challenges/ex1000.py
+++
@@ -1,9 +0,0 @@
-# This is how to write a Python module
-
-def main():
-	#do things
-	print('This is the main function')
-
-
-if __name__ == "__main__":
-    main()
