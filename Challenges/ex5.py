bdba447ecefe586c217a05a7bd177bcfdc15fa60
alistairwalsh <alistair.walsh@me.com>
2016-06-06 14:14:26 +1000

    small mod to text of challenge

---
+++ Challenges/ex5.py
@@ -0,0 +1,12 @@
+# I'm doing some marking and want an easy way to get the letter grade for any mark
+
+# Write a script called letter_grade.py that prints the correct letter grade given a mark
+
+# Score Grade
+# >= 0.9 A
+# >= 0.8 B
+# >= 0.7 C
+# >= 0.6 D
+# < 0.6 F
+
+#extra points for tests
